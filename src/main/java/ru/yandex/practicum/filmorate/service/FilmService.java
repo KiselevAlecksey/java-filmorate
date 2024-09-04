6a07a9e09be8a9859fe44001be8686eccd9e16b3
Mofo328 <163290172+Mofo328@users.noreply.github.com>
2024-09-04 19:35:27 +0500

    поправил чекстайл

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -21,7 +21,7 @@ public interface FilmService {
 
     boolean removeLike(Long filmId, Long userId);
 
-    Collection<FilmDto> getPopularFilms(Optional<Integer> countOpt) ;
+    Collection<FilmDto> getPopularFilms(Optional<Integer> countOpt);
 
     Collection<FilmDto> findAll();
 
