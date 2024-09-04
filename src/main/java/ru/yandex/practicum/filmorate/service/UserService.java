ae9ae545b4287704abc1a9129103b93b4b4a1f86
Mofo328 <163290172+Mofo328@users.noreply.github.com>
2024-09-04 18:37:41 +0500

    поправил чекстайл

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -4,11 +4,9 @@ import ru.yandex.practicum.filmorate.dto.user.NewUserRequest;
 import ru.yandex.practicum.filmorate.dto.user.UpdateUserRequest;
 import ru.yandex.practicum.filmorate.dto.user.UserDto;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
 
 public interface UserService {
 
